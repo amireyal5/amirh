3e54a4a6b5e40d85aa065c5fa9f04e58231d191b
amireyal5 <amirher@gmail.com>
2025-06-11 15:28:26 +0300

    Update menu.js

--- static/js/menu.js
+++ static/js/menu.js
@@ -1,25 +1,11 @@
-'use strict';
+document.addEventListener('DOMContentLoaded', () => {
+  const hamburgerToggle = document.querySelector('.hamburger-menu-toggle');
+  const mobileMenu = document.querySelector('.mobile-nav-menu');
 
-(function iifeMenu(document, window, undefined) {
-	var menuBtn = document.querySelector('.menu__btn');
-	var	menu = document.querySelector('.menu__list');
-
-	function toggleMenu() {
-		menu.classList.toggle('menu__list--active');
-		menu.classList.toggle('menu__list--transition');
-		this.classList.toggle('menu__btn--active');
-		this.setAttribute(
-			'aria-expanded',
-			this.getAttribute('aria-expanded') === 'true' ? 'false' : 'true'
-		);
-	}
-
-	function removeMenuTransition() {
-		this.classList.remove('menu__list--transition');
-	}
-
-	if (menuBtn && menu) {
-		menuBtn.addEventListener('click', toggleMenu, false);
-		menu.addEventListener('transitionend', removeMenuTransition, false);
-	}
-}(document, window));
+  if (hamburgerToggle && mobileMenu) {
+    hamburgerToggle.addEventListener('click', () => {
+      mobileMenu.classList.toggle('is-open'); // החלף מחלקה
+      hamburgerToggle.classList.toggle('is-active'); // אופציונלי: לשנות את אייקון ההמבורגר ל-X
+    });
+  }
+});
