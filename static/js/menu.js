fdb99f0ec92b87f0500cb1bdc86df79ad689084e
amireyal5 <amirher@gmail.com>
2025-06-12 13:35:52 +0300

    Update menu.js

--- static/js/menu.js
+++ static/js/menu.js
@@ -1,25 +1,31 @@
-'use strict';
+// assets/js/menu.js
 
-(function iifeMenu(document, window, undefined) {
-	var menuBtn = document.querySelector('.menu__btn');
-	var	menu = document.querySelector('.menu__list');
+document.addEventListener('DOMContentLoaded', function() {
+    const menuButton = document.querySelector('.menu__btn');
+    const menuList = document.querySelector('.menu__list');
 
-	function toggleMenu() {
-		menu.classList.toggle('menu__list--active');
-		menu.classList.toggle('menu__list--transition');
-		this.classList.toggle('menu__btn--active');
-		this.setAttribute(
-			'aria-expanded',
-			this.getAttribute('aria-expanded') === 'true' ? 'false' : 'true'
-		);
-	}
+    if (menuButton && menuList) {
+        // Toggle main menu visibility when hamburger button is clicked
+        menuButton.addEventListener('click', function() {
+            const isExpanded = this.getAttribute('aria-expanded') === 'true' || false;
+            this.setAttribute('aria-expanded', !isExpanded);
+            menuList.classList.toggle('menu__list--open');
+        });
 
-	function removeMenuTransition() {
-		this.classList.remove('menu__list--transition');
-	}
+        // Close main menu when any menu item link is clicked
+        menuList.querySelectorAll('.menu__item a.menu__link').forEach(link => {
+            link.addEventListener('click', () => {
+                menuButton.setAttribute('aria-expanded', 'false');
+                menuList.classList.remove('menu__list--open');
+            });
+        });
 
-	if (menuBtn && menu) {
-		menuBtn.addEventListener('click', toggleMenu, false);
-		menu.addEventListener('transitionend', removeMenuTransition, false);
-	}
-}(document, window));
+        // Close menu if clicked outside of the menu or hamburger button
+        document.addEventListener('click', function(event) {
+            if (!menuList.contains(event.target) && !menuButton.contains(event.target)) {
+                menuButton.setAttribute('aria-expanded', 'false');
+                menuList.classList.remove('menu__list--open');
+            }
+        });
+    }
+});
